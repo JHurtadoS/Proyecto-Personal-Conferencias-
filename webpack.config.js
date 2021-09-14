d2dd1e4121d619fa42103614bb9b3681e3a7e307
JHurtadoS <juaneshs2014@gmail.com>
2021-09-14 17:37:19 -0500

    commit on ModeloBasico

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,62 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
 const devMode = process.env.NODE_ENV !== "production";
+
 module.exports = {
+
+    devtool: "source-map",
     mode: 'development',
     entry: './src/js/main.js',
     watch: true,
 
+
     output: {
         path: path.resolve(__dirname + '/build'),
         filename: '[name].js',
-        filename: 'bundle.js'
+        filename: 'js/bundle.js'
+    },
+
+    module: {
+
+        rules: [{
+            test: /\.(jpe?g)$/i,
+            type: "asset",
+        }, ],
     },
     plugins: [
         new MiniCssExtractPlugin({
-
-            filename: "[name].css",
+            filename: "css/[name].css",
             chunkFilename: "[id].css",
         }),
+        new ImageMinimizerPlugin({
+            test: /\.(jpe?g)$/i,
+            filename: "img/[name].webp",
+            minimizerOptions: {
+                plugins: ["imagemin-webp"],
+            },
+        }),
+
     ],
     module: {
         rules: [{
-            test: /\.s[ac]ss$/i,
 
-            use: [{
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        publicPath: '',
-                    }
-                },
-                {
-                    loader: "css-loader",
-                    options: { sourceMap: true }
-                }, {
-                    loader: "sass-loader",
-                    options: { sourceMap: true }
-                }
-            ]
+            test: /.s?css$/,
+            use: [MiniCssExtractPlugin.loader,
+                { loader: "css-loader", options: { sourceMap: true } },
+                { loader: "sass-loader", options: { sourceMap: true } },
+            ],
         }, ],
     },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
+
+    },
+
 
 };
